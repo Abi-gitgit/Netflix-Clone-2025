3d1a429e9489d490e89488678e920ee283866fb5
Soli12 <kehaliabi@gmail.com>
2025-05-13 01:37:22 -0400

    header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import './App.css';
+import Home from "./Pages/Home/Home";
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <hi>Hello</hi>
+
+      <Home />
+      
     </div>
+
   );
 }
 
